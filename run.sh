add01d559c0fdc8ea923725520cc14bfeb0b09b6
Emil Bajit <ebajit@gmail.com>
2025-09-17 20:23:42 -0400

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Run script for the Tiger-IR optimizer
-# This script takes 2 command line arguments:
+# This script takes 1 command line argument:
 # $1 - path to the input IR file
-# $2 - path where the output IR file should be created
 
 # Check if correct number of arguments provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <input_ir_file> <output_ir_file>"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <input_ir_file>"
     exit 1
 fi
 
 # Run the optimizer
-java -cp ./build Main "$1" "$2"
+java -cp ./build Main "$1" "out.ir"
